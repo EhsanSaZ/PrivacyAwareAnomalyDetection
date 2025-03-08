9c6cbf339f9934b4796906a9820231efb2cee25c
Ehsan Saeedizade <e24saeedi@gmail.com>
2025-03-08 09:56:21 -0800

    Add experiment configs

--- XgBoost_flower_federated_bottleneck_detection/experiment_config.py
+++ XgBoost_flower_federated_bottleneck_detection/experiment_config.py
@@ -27,15 +27,15 @@ meta_args.model = "mlp"
 # meta_args.batch_size = 100
 # meta_args.decay_weight = 1.0
 # meta_args.data_type = ""
-meta_args.round = 80 # 50
-meta_args.epoch_iterations = 20
+meta_args.round = 5 # 50
+meta_args.epoch_iterations = 10
 meta_args.local_lr = 0.001
 meta_args.batch_size = 150
 meta_args.decay_weight = 1.0
 meta_args.data_type = ""
 
 meta_args.remove_labels = [17, 21, 25, 29]
-meta_args.features = ['sender_avg_rtt_value', 'sender_retrans', 'sender_segs_in', 'sender_tcp_snd_buffer_max','sender_nic_send_bytes', 'sender_nic_receive_bytes',
+meta_args.features = ['sender_cwnd_rate', 'sender_avg_rtt_value', 'sender_retrans', 'sender_segs_in', 'sender_tcp_snd_buffer_max','sender_nic_send_bytes', 'sender_nic_receive_bytes',
             'receiver_seg_out', 'receiver_tcp_rcv_buffer_max', 'receiver_nic_send_bytes', 'receiver_nic_receive_bytes', 'sender_remote_ost_read_bytes', 'receiver_remote_ost_write_bytes']  
 meta_args.filenames = { 
     "wisconsin_ssd_merged": "./ds/v3/selected_cols_merged/wisconsin-220g2-10Gbps_ssd_merged_V3.csv",
@@ -49,20 +49,29 @@ meta_args.filenames = {
     "utah_ssd_delay_10ms_merged":"./ds/v3/selected_cols_merged/utah-6525-25-ssd-delayed-10ms_merged_V3.csv",
     }
 
+# meta_args.TR_enabled = False
+# meta_args.save_dir = "data_loaders"
+# meta_args.log_path = "results/XGBoost_FedXgbBaggin_WTR"
+
+
+meta_args.TR_enabled = True
+meta_args.save_dir = "data_loaders_TR_2"
+meta_args.log_path = "results/XGBoost_FedXgbBaggin_TR"
+
 meta_args.local_epochs = 1
 meta_args.params = {
     "objective": "multi:softmax",
     # "objective": "multi:softprob",
     "eta": 0.1,
     # "n_estimators": 500,
-    "max_depth": 8,
+    "max_depth": 6,
     "eval_metric": "mlogloss",
     # "eval-metric": "auc",
     # "eval-metric": "merror",
     "nthread": 16,
     "num_parallel_tree": 1,
     "subsample": 0.8,
-    "tree_method": "hist"
+    "tree_method": "hist",
     }
 
 
